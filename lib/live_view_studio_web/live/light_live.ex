7aad441939d022d54f3c3fe5793f221ee98ab1aa
Matt Willy <matt.willy@gmail.com>
2023-02-09 08:22:57 -0700

    create a liveview page that uses buttons

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,57 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, brightness: 10)
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}>
+          <%= @brightness %>%
+        </span>
+      </div>
+      <button phx-click="off">
+        <img src="/images/light-off.svg" />
+      </button>
+      <button phx-click="down">
+        <img src="/images/down.svg" alt="" />
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" alt="" />
+      </button>
+      <button phx-click="on">
+        <img src="/images/light-on.svg" />
+      </button>
+      <button phx-click="party">
+        <img src="/images/fire.svg" />
+      </button>
+    </div>
+    """
+  end
+
+  def handle_event("off", _, socket) do
+    {:noreply, assign(socket, brightness: 0)}
+  end
+
+  def handle_event("up", _, socket) do
+    {:noreply, update(socket, :brightness, &min(100, &1 + 10))}
+  end
+
+  def handle_event("down", _, socket) do
+    {:noreply, update(socket, :brightness, &max(0, &1 - 10))}
+  end
+
+  def handle_event("on", _, socket) do
+    {:noreply, assign(socket, brightness: 100)}
+  end
+
+  def handle_event("party", _, socket) do
+    {:noreply, assign(socket, brightness: Enum.random(0..100))}
+  end
+end
