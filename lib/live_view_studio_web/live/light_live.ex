519cac47c5e7a0353339c5b594c0b69d8a52a30e
Matt Willy <matt.willy@gmail.com>
2023-02-10 09:50:35 -0700

    exercises for form inputs

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, brightness: 10)
+    socket = assign(socket, brightness: 10, temperature: "3000")
 
     {:ok, socket}
   end
@@ -12,7 +12,7 @@ defmodule LiveViewStudioWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}>
+        <span style={"width: #{@brightness}%; background: #{temp_color(@temperature)}"}>
           <%= @brightness %>%
         </span>
       </div>
@@ -31,6 +31,29 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="party">
         <img src="/images/fire.svg" />
       </button>
+
+      <form phx-change="update">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          name="brightness"
+          value={@brightness}
+        />
+      </form>
+
+      <form phx-change="color-update">
+        <%= for temp <- ["3000", "4000", "5000"] do %>
+          <input
+            type="radio"
+            id={temp}
+            name="temp"
+            value={temp}
+            checked={@temperature == temp}
+          />
+          <label for={temp}><%= temp %></label>
+        <% end %>
+      </form>
     </div>
     """
   end
@@ -54,4 +77,16 @@ defmodule LiveViewStudioWeb.LightLive do
   def handle_event("party", _, socket) do
     {:noreply, assign(socket, brightness: Enum.random(0..100))}
   end
+
+  def handle_event("update", %{"brightness" => brightness}, socket) do
+    {:noreply, assign(socket, brightness: brightness)}
+  end
+
+  def handle_event("color-update", %{"temp" => temp}, socket) do
+    {:noreply, assign(socket, temperature: temp)}
+  end
+
+  defp temp_color("3000"), do: "#F1C40D"
+  defp temp_color("4000"), do: "#FEFF66"
+  defp temp_color("5000"), do: "#99CCFF"
 end
