10aebe434548ce3bfc6e780eca32cf5344727dc0
Matt Willy <matt.willy@gmail.com>
2023-02-23 16:44:13 -0700

    pull the filter and athletes into a function component

--- lib/live_view_studio_web/live/athletes_live.ex
+++ lib/live_view_studio_web/live/athletes_live.ex
@@ -15,39 +15,10 @@ defmodule LiveViewStudioWeb.AthletesLive do
     ~H"""
     <h1>Athletes</h1>
     <div id="athletes">
-      <form phx-change="filter">
-        <div class="filters">
-          <select name="sport">
-            <%= Phoenix.HTML.Form.options_for_select(
-              sport_options(),
-              @filter.sport
-            ) %>
-          </select>
-          <select name="status">
-            <%= Phoenix.HTML.Form.options_for_select(
-              status_options(),
-              @filter.status
-            ) %>
-          </select>
-        </div>
-      </form>
+      <.filter_form filter={@filter} />
+
       <div class="athletes">
-        <div :for={athlete <- @athletes} class="athlete">
-          <div class="emoji">
-            <%= athlete.emoji %>
-          </div>
-          <div class="name">
-            <%= athlete.name %>
-          </div>
-          <div class="details">
-            <span class="sport">
-              <%= athlete.sport %>
-            </span>
-            <span class="status">
-              <%= athlete.status %>
-            </span>
-          </div>
-        </div>
+        <.athlete :for={athlete <- @athletes} athlete={athlete} />
       </div>
     </div>
     """
@@ -56,6 +27,50 @@ defmodule LiveViewStudioWeb.AthletesLive do
   def handle_event("filter", %{"sport" => sport, "status" => status}, socket) do
     filter = %{sport: sport, status: status}
     {:noreply, assign(socket, filter: filter, athletes: Athletes.list_athletes(filter))}
+  end
+
+  attr :filter, :map, required: true
+
+  defp filter_form(assigns) do
+    ~H"""
+    <form phx-change="filter">
+      <div class="filters">
+        <select name="sport">
+          <%= Phoenix.HTML.Form.options_for_select(
+            sport_options(),
+            @filter.sport
+          ) %>
+        </select>
+        <select name="status">
+          <%= Phoenix.HTML.Form.options_for_select(
+            status_options(),
+            @filter.status
+          ) %>
+        </select>
+      </div>
+    </form>
+    """
+  end
+
+  attr :athlete, :map, required: true
+
+  defp athlete(assigns) do
+    ~H"""
+    <div class="emoji">
+      <%= @athlete.emoji %>
+    </div>
+    <div class="name">
+      <%= @athlete.name %>
+    </div>
+    <div class="details">
+      <span class="sport">
+        <%= @athlete.sport %>
+      </span>
+      <span class="status">
+        <%= @athlete.status %>
+      </span>
+    </div>
+    """
   end
 
   defp sport_options do
