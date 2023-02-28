9604b5a436d483a34089bdc6f0eb6fa1fd0f3165
Matt Willy <matt.willy@gmail.com>
2023-02-28 09:04:17 -0700

    move the render into a template

--- lib/live_view_studio_web/live/flights_live.ex
+++ lib/live_view_studio_web/live/flights_live.ex
@@ -12,63 +12,7 @@ defmodule LiveViewStudioWeb.FlightsLive do
         loading: false
       )
 
-    {:ok, socket}
-  end
-
-  def render(assigns) do
-    ~H"""
-    <h1>Find a Flight</h1>
-    <div id="flights">
-      <form phx-submit="search" phx-change="suggest">
-        <input
-          type="text"
-          name="airport"
-          value={@airport}
-          placeholder="Airport Code"
-          autofocus
-          autocomplete="off"
-          readonly={@loading}
-          list="matches"
-          phx-debounce="300"
-        />
-
-        <button>
-          <img src="/images/search.svg" />
-        </button>
-      </form>
-
-      <datalist id="matches">
-        <option :for={{code, name} <- @matches} value={code}>
-          <%= name %>
-        </option>
-      </datalist>
-
-      <.loading loading={@loading} />
-
-      <div class="flights">
-        <ul>
-          <li :for={flight <- @flights}>
-            <div class="first-line">
-              <div class="number">
-                Flight #<%= flight.number %>
-              </div>
-              <div class="origin-destination">
-                <%= flight.origin %> to <%= flight.destination %>
-              </div>
-            </div>
-            <div class="second-line">
-              <div class="departs">
-                Departs: <%= flight.departure_time %>
-              </div>
-              <div class="arrives">
-                Arrives: <%= flight.arrival_time %>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    """
+    {:ok, socket, temporary_assigns: [flights: []]}
   end
 
   def handle_event("search", %{"airport" => airport}, socket) do
--- lib/live_view_studio_web/live/sales_live.ex
+++ lib/live_view_studio_web/live/sales_live.ex
@@ -11,44 +11,6 @@ defmodule LiveViewStudioWeb.SalesLive do
     {:ok, assign_sales_data(socket)}
   end
 
-  def render(assigns) do
-    ~H"""
-    <h1>Snappy Sales 📊</h1>
-    <div id="sales">
-      <div class="stats">
-        <div class="stat">
-          <span class="value">
-            <%= @new_orders %>
-          </span>
-          <span class="label">
-            New Orders
-          </span>
-        </div>
-        <div class="stat">
-          <span class="value">
-            $<%= @sales_amount %>
-          </span>
-          <span class="label">
-            Sales Amount
-          </span>
-        </div>
-        <div class="stat">
-          <span class="value">
-            <%= @satisfaction %>%
-          </span>
-          <span class="label">
-            Satisfaction
-          </span>
-        </div>
-      </div>
-
-      <button phx-click="refresh">
-        <img src="/images/refresh.svg" /> Refresh
-      </button>
-    </div>
-    """
-  end
-
   def handle_event("refresh", _, socket) do
     {:noreply, assign_sales_data(socket)}
   end
