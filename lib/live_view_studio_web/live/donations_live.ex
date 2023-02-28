27c29fb5685617c0be3dc1932937c5d16f218108
Matt Willy <matt.willy@gmail.com>
2023-02-28 09:04:04 -0700

    add sorting to the donations live page

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,38 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
     {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{sort_by: sort_by, sort_order: sort_order}
+
+    donations = Donations.list_donations(options)
+
+    {:noreply, assign(socket, donations: donations, options: options)}
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
+  end
+
+  slot :inner_block, required: true
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+
+  defp sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
   end
 end
