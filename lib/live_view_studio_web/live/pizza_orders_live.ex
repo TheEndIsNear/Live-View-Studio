60382d1784fe012feb46ea92d56ae03e424c367b
Matt Willy <matt.willy@gmail.com>
2023-03-03 16:54:26 -0700

    add sorting to the pizza-orders page

--- lib/live_view_studio_web/live/pizza_orders_live.ex
+++ lib/live_view_studio_web/live/pizza_orders_live.ex
@@ -5,11 +5,61 @@ defmodule LiveViewStudioWeb.PizzaOrdersLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
+    {:ok, socket, temporary_assigns: [pizza_orders: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_order = valid_sort_order(params)
+    sort_by = valid_sort_by(params)
+
+    options = %{sort_order: sort_order, sort_by: sort_by}
+
     socket =
       assign(socket,
-        pizza_orders: PizzaOrders.list_pizza_orders()
+        pizza_orders: PizzaOrders.list_pizza_orders(options),
+        options: options
       )
 
-    {:ok, socket, temporary_assigns: [pizza_orders: []]}
+    {:noreply, socket}
+  end
+
+  defp next_sort_order(:asc), do: :desc
+  defp next_sort_order(:desc), do: :asc
+
+  defp valid_sort_order(%{"sort_order" => sort_order}) when sort_order in ~w(:asc desc) do
+    String.to_existing_atom(sort_order)
+  end
+
+  defp valid_sort_order(_), do: :asc
+
+  defp valid_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(id size style topping_1 topping_2 price) do
+    String.to_existing_atom(sort_by)
+  end
+
+  defp valid_sort_by(_), do: :id
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "↑"
+      :desc -> "↓"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
+
+  attr :sort_order, :atom, required: true
+  attr :sort_by, :atom, required: true
+  slot :inner_block, required: true
+
+  defp sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: next_sort_order(@sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
   end
 end
